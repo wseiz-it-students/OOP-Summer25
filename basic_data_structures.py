0bf03f07de7ea7e0c5a0bbeb3b0195dd6d64a8ca
efeadil <153407417+efeadil@users.noreply.github.com>
2025-03-11 11:26:30 +0100

    Create basic_data_structures.py

---
+++ basic_data_structures.py
@@ -0,0 +1,16 @@
+# Integer
+my_age = 22  # int
+# Floating point
+my_height = 7.4 # float
+# String
+my_name = "adil"  # str
+# List
+my_favorite_numbers = [5, 14, 21]  # list
+# Dictionary
+my_profile = {"name": "adil", "age": 22}  # dict
+# Tuple
+my_coordinates = (40.7128, -74.0060)  # tup
+# Set
+my_unique_numbers = {1, 2, 3, 4}  # set
+# Boolean
+is_student = True  # bool
