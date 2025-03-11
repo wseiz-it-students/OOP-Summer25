664a29ef34aa49d4f7f5a89fc11c68696cde850f
carlsberk <137445298+carlsberk@users.noreply.github.com>
2025-03-11 11:40:09 +0100

    Update basic_data_structures.py

--- basic_data_structures.py
+++ basic_data_structures.py
@@ -4,3 +4,6 @@ number = 2 + 55   # complex
 is_active = True   # bool
 name = "Berk"       # str
 empty = None       # NoneType
+git add .
+git commit -m 'Assign data structures to variables'
+git push origin main
