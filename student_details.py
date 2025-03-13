64c2eac6cfdca53df6dc8dd84e67834113f86af5
efeadil <153407417+efeadil@users.noreply.github.com>
2025-03-13 10:24:47 +0100

    Update student_details.py

--- student_details.py
+++ student_details.py
@@ -1,1 +1,13 @@
 
+ my_age = 22  
+ my_indexnumber = 35480
+ my_name = "adil efe "
+ my_surname = "begen"
+ nationality = "turja"
+ starting_date = "01.01.2025"
+ courses = IT
+
+
+
+
+my_profile = {"name": "adil", "age": 22}  
