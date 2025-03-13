93e65c22e5d1ba6ca015be4ffb8c557743a03c93
efeadil <153407417+efeadil@users.noreply.github.com>
2025-03-13 10:42:38 +0100

    Create student_details_structure.py

---
+++ student_details_structure.py
@@ -0,0 +1,11 @@
+student1 = {
+    "first_name": "adil",
+    "last_name": "begen",
+    "index_number": "35480",
+    "nationality": "turk",
+    "starting_date": "2024-03-13",
+    "courses": ["Mathematics", "Physics", "Computer Science"]
+}
+
+
+print(f"Student Details:\nFirst Name: {student1['first_name']}\nLast Name: {student1['last_name']}\nIndex Number: {student1['index_number']}\nNationality: {student1['nationality']}\nStarting Date: {student1['starting_date']}\nCourses: {', '.join(student1['courses'])}")
