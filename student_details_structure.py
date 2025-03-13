edfee2a9cb36f2a1148156073c22dece40e8ec19
Cemkazimkaya35412 <kayacem982@gmail.com>
2025-03-13 11:43:03 +0000

    new details

--- student_details_structure.py
+++ student_details_structure.py
@@ -1,1 +1,52 @@
+student1 ={
 
+
+first_name = "Cem"
+last_name = "Kaya"
+index_number = "35412"
+nationality = "turkish"
+strating_date = 01\03\2025
+courses = "[comptuer engineering]"
+}
+print(student1["first_name"])
+print(student1["last_name"])
+print(student1["index_number"])
+print(student1["nationality"])
+print(student1["starting_date"])
+print(student1["courses"] 
+
+student2 ={
+
+
+first_name = "john"
+last_name = "kennedy"
+index_number = "3674"
+nationality = "american"
+strating_date = 18\05\2025
+courses = "[comptuer engineering]"
+}
+
+print(student2["first_name"])
+print(student2["last_name"])
+print(student2["index_number"])
+print(student2["nationality"])
+print(student2["starting_date"])
+print(student2["courses"] 
+
+
+student3 ={
+
+
+first_name = "pablo"
+last_name = "esteban"
+index_number = "4325"
+nationality = "brazilian"
+strating_date = "29\10\2024
+courses = "[comptuer engineering]"
+}
+print(student3["first_name"])
+print(studen3["last_name"])
+print(student3["index_number"])
+print(student3["nationality"])
+print(student3["starting_date"])
+print(student3["courses"] 
